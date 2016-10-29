2bd2a198a4691ed8e237e778522e67024ed94aa3
CandleClockwork <candleclockwork@gmail.com>
2016-10-29 16:39:57 +0200

    Add files via upload

---
+++ src/view/conio.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <termios.h>
+#include <unistd.h>
+
+using namespace std;
+
+int getch(void)
+{
+	struct termios oldt, newt;
+	int ch;
+
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	ch = getchar();
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+	return ch;
+}
+
+return 0;
+}
