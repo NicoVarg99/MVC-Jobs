7a0f1e2834fd83b70f78b66231bd8ea0e17b55f9
Carlo Ramponi <magocarlos1999@gmail.com>
2016-10-29 07:29:08 +0200

    Creazione prototipo della funzione 'model_listStudents'

--- src/model/model.cpp
+++ src/model/model.cpp
@@ -1,2 +1,20 @@
+#include "model.h"
+
+//check if file exists
+//if exists
+				//list all students with their current job
+				//
+				//while(model_isDataAvailable){
+				//      view_printStudentList(model_getData("firstName"),model_getData("lastName"),mod$
+				//      model_pop();
+				//}
+				//NOTE: Do not print anything. That is view's job
+				//      Ask Mirco for adequate functions if needed.
+//if not
+				//print an error message
+				//view_error("Data file not found.");
 
 
+int model_listStudents(){
+	
+}
--- src/model/model.h
+++ src/model/model.h
@@ -1,5 +1,7 @@
 #ifndef MODEL_H_INCLUDED
 
-	
+using namespace std;
+
+int model_listStudents();
 
 #endif
