243459192c8175d80bffdd5d91e5b1ec77fa46e8
Nicola Salsotto <nicola@cryptolab.net>
2016-10-29 14:15:40 +0200

    Update data.h

--- src/data.h
+++ src/data.h
@@ -1,19 +1,16 @@
 struct Job{
 	char name[50];
 	char desc[1000];
-	int status;
+	int jobStatus;
+	        //0 - No job assigned
+                //10 - Job assigned
+                //20 - Job started
+                //30 - Job completed
 };
 
 struct Student{
 	char firstName[50];
 	char lastName[50];
-
-	int jobStatus;
-		//0 - No job assigned
-		//10 - Job assigned
-		//20 - Job started
-		//30 - Job completed
-
-	int jobsNum;
+	int jobsNum; //Number of jobs of the Student
 	struct Job *job;
 };
