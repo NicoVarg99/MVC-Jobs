7b936eae6f9ee8a00c668da43029905ccfdc1c9c
Nicola Salsotto <nicola@cryptolab.net>
2016-10-31 13:32:22 +0100

    Restore view.h from 118db54

---
+++ src/view/view.h
@@ -0,0 +1,11 @@
+#ifndef VIEW_H_INCLUDED
+#define VIEW_H_INCLUDED
+
+    int view_mainMenu();
+    void view_printError(const std::string topr);
+    void view_printDebug(const std::string dastampare);
+    void view_printMsg(char *topr);
+    void view_printWaitMessage();
+
+
+#endif // VIEW_H_INCLUDED
